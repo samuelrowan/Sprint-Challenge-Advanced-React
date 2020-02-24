8afc52596a0d1def77c3031b9acb9100eddc34b6
Samuel Rowan <samuel@samuelrowan.com>
2020-02-23 19:16:40 -0600

    Added foundations for dark mode.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CompetitorList from './components/CompetitorList'
 import './App.css';
+import useDarkMode from './hooks/useDarkMode';
 
 function App() {
   return (
     <div className="App">
+      <button>toggle dark mode</button>
       <CompetitorList />
     </div>
   );
